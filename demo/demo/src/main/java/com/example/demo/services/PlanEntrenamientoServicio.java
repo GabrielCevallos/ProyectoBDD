74bda0f62910ce4ee94c3f4d2e5976a84a093928
Iván Fernández <ivan.fernandez@unl.edu.ec>
2025-02-05 17:57:13 -0500

    Rama Ivan Clases

--- demo/demo/src/main/java/com/example/demo/services/PlanEntrenamientoServicio.java
+++ demo/demo/src/main/java/com/example/demo/services/PlanEntrenamientoServicio.java
@@ -1,5 +1,32 @@
 package com.example.demo.services;
 
+import com.example.demo.models.PlanEntrenamiento;
+import com.example.demo.repository.PlanEntrenamientoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
 public class PlanEntrenamientoServicio {
-    
-}
+
+        @Autowired
+        private PlanEntrenamientoRepositorio planEntrenamientoRepositorio;
+
+        public List<PlanEntrenamiento> listarPlanesEntrenamiento() {
+            return planEntrenamientoRepositorio.findAll();
+        }
+
+        public PlanEntrenamiento buscarPlanEntrenamiento(Long id) {
+            return planEntrenamientoRepositorio.findById(id).orElse(null);
+        }
+
+        public PlanEntrenamiento guardarPlanEntrenamiento(PlanEntrenamiento planEntrenamiento) {
+            return planEntrenamientoRepositorio.save(planEntrenamiento);
+        }
+
+        public void eliminarPlanEntrenamiento(Long id) {
+            planEntrenamientoRepositorio.deleteById(id);
+        }
+    }
+
