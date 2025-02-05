6ec70ff34bfce69a453fe79589b2960977cc3d80
PanchoJc <francisco.jaramillo@unl.edu.ec>
2025-02-05 17:29:53 -0500

    Persona y telefono servicios

--- demo/demo/src/main/java/com/example/demo/services/PersonaServicio.java
+++ demo/demo/src/main/java/com/example/demo/services/PersonaServicio.java
@@ -1,5 +1,27 @@
-package com.example.demo.services;
-
+import com.example.demo.models.Persona;
+import com.example.demo.repository.PersonaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 public class PersonaServicio {
+
+    @Autowired
+    private PersonaRepositorio personaRepositorio;
+
+    public List<Persona> listarPersonas() {
+        return personaRepositorio.findAll();
+    }
+
+    public Persona buscarPersona(Long id) {
+        return personaRepositorio.findById(id).orElse(null);
+    }
+
+    public Persona guardarPersona(Persona persona) {
+        return personaRepositorio.save(persona);
+    }
+
+    public void eliminarPersona(Long id) {
+        personaRepositorio.deleteById(id);
+    }
     
 }
